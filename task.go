3b657d5fe820ddedbfdfd48a5ec72642c94b6a9c
Ryan Berckmans <ryanberckmans@gmail.com>
2017-11-28 08:23:29 -0500

    Add tasks filter functions

--- task.go
+++ task.go
@@ -6,6 +6,8 @@ import (
 	"github.com/google/uuid"
 )
 
+type tasks []task
+
 type task struct {
 	ID             uuid.UUID
 	CreatedAt      time.Time
@@ -24,4 +26,41 @@ func newTask() *task {
 
 func (t *task) isDone() bool {
 	return len(t.Timeline) > 0 && len(t.Timeline)%2 == 0
+}
+
+func (ts tasks) notDeleted() tasks {
+	return filterTasks(ts, func(t *task) bool {
+		return !t.IsDeleted
+	})
+}
+
+func (ts tasks) notDone() tasks {
+	return filterTasks(ts, func(t *task) bool {
+		return !t.isDone()
+	})
+}
+
+func (ts tasks) notStarted() tasks {
+	return filterTasks(ts, func(t *task) bool {
+		return len(t.Timeline) == 0
+	})
+}
+
+func (ts tasks) estimated() tasks {
+	return filterTasks(ts, func(t *task) bool {
+		return t.EstimatedHours > 0
+	})
+}
+
+func filterTasks(ts []task, fn func(t *task) bool) []task {
+	if ts == nil {
+		return nil
+	}
+	var res []task
+	for i := range ts {
+		if fn(&ts[i]) {
+			res = append(res, ts[i])
+		}
+	}
+	return res
 }
