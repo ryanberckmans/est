7476ec399cf350aa64ca5b44b9c67a261c60eb7c
Ryan Berckmans <ryanberckmans@gmail.com>
2017-12-05 07:11:57 -0500

    Add 'est add' cmd

--- cmd/add.go
+++ cmd/add.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"regexp"
+	"strconv"
 	"strings"
 
+	"github.com/ryanberckmans/est/core"
 	"github.com/spf13/cobra"
 )
 
@@ -37,11 +42,66 @@ Examples:
   est add fix the bug -e 0.5d
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		name := strings.Join(args, " ")
-		fmt.Println("add called")
-		fmt.Printf("args %d %+v\n", len(args), args)
-		fmt.Printf("name %s\n", name)
+		name := strings.TrimSpace(strings.Join(args, " "))
+		if len(name) < 1 {
+			fmt.Println("fatal: no task name given")
+			os.Exit(1)
+			return
+		}
+		estimate, err := parseEstimate(addCmdNewTaskEstimate)
+		if err != nil {
+			fmt.Println("fatal: " + err.Error())
+			os.Exit(1)
+			return
+		}
+		core.WithEstConfigAndFile(func(ec *core.EstConfig, ef *core.EstFile) {
+			t := core.NewTask()
+			t.Name = name
+			if estimate != 0 {
+				t.Hours = []float64{estimate}
+			}
+			ef.Tasks = append(ef.Tasks, *t)
+			if err := ef.Write(); err != nil {
+				fmt.Printf("fatal: %v\n", err)
+				os.Exit(1)
+				return
+			}
+			fmt.Println(core.RenderTaskOneLineSummary(t))
+		}, func() {
+			// failed to load estconfig or estfile. Err printed elsewhere.
+		})
 	},
+}
+
+var estimateRegexp = regexp.MustCompile(`^([1-9](\.[0-9]*)?$|^[0-9]\.[0-9]+)(h|d|w)?$`)
+
+func parseEstimate(e string) (float64, error) {
+	if e == "" {
+		return 0, nil
+	}
+	if !estimateRegexp.MatchString(e) {
+		return 0, errors.New("invalid estimate")
+	}
+	unitMultiplier := 1.0 // default to hours
+	var eWithoutUnit string
+	switch e[len(e)-1:] {
+	case "h":
+		eWithoutUnit = e[:len(e)-1]
+	case "d":
+		eWithoutUnit = e[:len(e)-1]
+		unitMultiplier = 8 // 8 hours in a day
+	case "w":
+		eWithoutUnit = e[:len(e)-1]
+		unitMultiplier = 8 * 5 // 40 hours in a week
+	default:
+		eWithoutUnit = e
+	}
+
+	f, err := strconv.ParseFloat(eWithoutUnit, 64)
+	if err != nil {
+		return 0, errors.New("estimate wasn't a float")
+	}
+	return f * unitMultiplier, nil
 }
 
 var addCmdNewTaskEstimate string
